d70877078c04114a63cc83523a1b7215baaa4445
Zachary McGuire <zacharymcguire@icloud.com>
2021-02-07 18:03:45 +1100

    added table dimensions to output

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def index():
             'index.html.jinja',
             form=form,
             table=df.to_html(classes='table table-striped table-bordered',
-                             header='true', index=False, justify="left")
+                             header='true', index=False, justify="left",
+                             show_dimensions=True)
         )
     return render_template('index.html.jinja', form=form)
